dd354e5c14ec106b65dd050451985c390be82395
tomstevns <tomstevns@gmail.com>
2022-04-08 10:22:44 +0200

    Win Unix enabled files

--- C-code/socket-client.c
+++ C-code/socket-client.c
@@ -1,22 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#ifdef _WIN32
+#include <windows.h>
+#include <winsock.h>
+struct WSAData wsaData;
+void init_socket_lib() { WSAStartup(MAKEWORD(1, 1), &wsaData); }
+#else
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #include <arpa/inet.h>
+typedef int SOCKET;
+#define INVALID_SOCKET -1
+#define closesocket(s) close(s)
+void init_socket_lib() {}
+#endif
 
 int main(void)
 {
-  int sockfd = 0,n = 0;
-  char recvBuff[1024];
+  SOCKET sockfd = INVALID_SOCKET;
+  int n = 0;
+  char recvBuff[1500]; // to fit better within the Ethernet MTU
   struct sockaddr_in serv_addr;
 
-  memset(recvBuff, '0' ,sizeof(recvBuff));
-  if((sockfd = socket(AF_INET, SOCK_STREAM, 0))< 0)
+  init_socket_lib();
+  memset(recvBuff, 0, sizeof(recvBuff));
+  if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
     {
       printf("\n Error : Could not create socket \n");
       return 1;
@@ -26,19 +39,19 @@ int main(void)
   serv_addr.sin_port = htons(5000);
   serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-  if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0)
+  if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)))
     {
       printf("\n Error : Connect Failed \n");
       return 1;
     }
 
-  while((n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
+  while((n = recv(sockfd, recvBuff, sizeof(recvBuff)-1, 0)) > 0)
     {
       recvBuff[n] = 0;
       if(fputs(recvBuff, stdout) == EOF)
-    {
-      printf("\n Error : Fputs error");
-    }
+      {
+        printf("\n Error : Fputs error");
+      }
       printf("\n");
     }
 
@@ -46,6 +59,9 @@ int main(void)
     {
       printf("\n Read Error \n");
     }
+
+  shutdown(sockfd, 2);  // shutdown input and output streams, i.e. close TCP connection
+  closesocket(sockfd);  // free fd
 
   return 0;
 }
--- C-code/socket-server.c
+++ C-code/socket-server.c
@@ -1,49 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef _WIN32
+#include <windows.h>
+#include <winsock.h>
+struct WSAData wsaData;
+void init_socket_lib() { WSAStartup(MAKEWORD(1, 1), &wsaData); }
+#else
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <string.h>
 #include <sys/types.h>
+typedef int SOCKET;
+#define INVALID_SOCKET -1
+#define closesocket(s) close(s)
+void init_socket_lib() {}
+#endif
 
 int main(void)
 {
-  int listenfd = 0,connfd = 0;
+  SOCKET listenfd = INVALID_SOCKET, connfd = INVALID_SOCKET;
 
   struct sockaddr_in serv_addr;
 
-  char sendBuff[1025];
+  char sendBuff[1500]; // to fit better within the Ethernet MTU
   int numrv;
 
+  init_socket_lib();
   listenfd = socket(AF_INET, SOCK_STREAM, 0);
   printf("socket retrieve success\n");
 
-  memset(&serv_addr, '0', sizeof(serv_addr));
-  memset(sendBuff, '0', sizeof(sendBuff));
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  memset(sendBuff, 0, sizeof(sendBuff));
 
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
   serv_addr.sin_port = htons(5000);
 
-  bind(listenfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr));
+  if(bind(listenfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr)) != 0)
+    {
+      printf("Failed to bind\n");
+      return 1;
+    }
 
-  if(listen(listenfd, 10) == -1){
+  if(listen(listenfd, 10) == -1)
+    {
       printf("Failed to listen\n");
-      return -1;
-  }
+      return 1;
+    }
 
   while(1)
     {
       connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL); // accept awaiting request
 
       strcpy(sendBuff, "Message from server");
-      write(connfd, sendBuff, strlen(sendBuff));
+      send(connfd, sendBuff, strlen(sendBuff), 0);
 
-      close(connfd);
+      shutdown(connfd, 2);  // shutdown input and output streams, i.e. close TCP connection
+      closesocket(connfd);  // free fd
+
       sleep(1);
     }
+
 
   return 0;
 }
